467f9c2eec11feafe8c370095a291d7eef79ce82
ZAOUAM Sirageddine <zaouam.sirageddine@hotmail.com>
2020-05-14 03:06:24 +0200

    Création de l'interface Command

---
+++ src/main/java/drawing/Command.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package drawing;
+
+/**
+ * Pattern Command
+ * @author ZAOUAM Sirageddine
+ * @version 1.0
+ */
+public interface Command {
+	 /**
+     * executer une commande.
+     */
+    void execute();
+}
